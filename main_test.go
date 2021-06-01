e63c97b3e21af73e6cfbe0bb2333220051876c52
Roberta <robertasousa714@gmail.com>
2021-06-01 16:44:16 -0300

    Testes unitários das funções getAllAccounts(), newDeposit() e getBalance().

--- main_test.go
+++ main_test.go
@@ -29,10 +29,10 @@ func Router() *mux.Router {
     myRouter.HandleFunc("/login", authLogin).Methods("POST") //Faz login
     myRouter.HandleFunc("/transfers", newTransfer).Methods("POST") //Realiza transferência
     myRouter.HandleFunc("/transfers", getAllTransfers) //Retorna todas transferências feitas pelo usuário logado
-    myRouter.HandleFunc("/accounts", newAccount).Methods("POST") //Cria nova conta
-    myRouter.HandleFunc("/accounts", getAllAccounts) //Retorna todas as contas cadastradas
-    myRouter.HandleFunc("/accounts/{ID}/balance", getBalance) //Retorna o saldo da conta que pertence ao ID informado
-    myRouter.HandleFunc("/deposits", newDeposit).Methods("POST") //Realiza um depósito em uma conta cadastrada
+    myRouter.HandleFunc("/accounts", newAccount).Methods("POST") //Cria nova conta OK
+    myRouter.HandleFunc("/accounts", getAllAccounts) //Retorna todas as contas cadastradas OK
+    myRouter.HandleFunc("/accounts/{ID}/balance", getBalance) //Retorna o saldo da conta que pertence ao ID informado OK
+    myRouter.HandleFunc("/deposits", newDeposit).Methods("POST") //Realiza um depósito em uma conta cadastrada OK
     return myRouter
 }
 
@@ -47,7 +47,7 @@ func TestGetMongoClient(t *testing.T) {
 func TestNewAccount(t *testing.T) {
 	//Simulamos a criação de uma conta
 		//Mude os dados caso teste mais de uma vez!
-	var jsonAcc = []byte(`{ "ID" : 1, "name" : "Fulano", "cpf" : "7777", "secret" : "12345", "balance" : 99.0, "created_at" : ""}`)
+	var jsonAcc = []byte(`{ "ID" : 3, "name" : "Fulano", "cpf" : "5555", "secret" : "12345", "balance" : 0.0, "created_at" : ""}`)
 	req, _ := http.NewRequest("POST", "/accounts", bytes.NewBuffer(jsonAcc))
 	req.Header.Set("Content-Type", "application/json")
 
@@ -57,7 +57,6 @@ func TestNewAccount(t *testing.T) {
 	//Pega os dados do response para realizar a validação
 	var m map[string]interface{}
     json.Unmarshal(response.Body.Bytes(), &m)
-    fmt.Println(m["name"])
 
     //Valida os dados passados com os dados do body
 
@@ -65,13 +64,54 @@ func TestNewAccount(t *testing.T) {
         t.Errorf("Expected name to be 'Fulano'. Got '%v'", m["name"])
     }
 
-    if m["cpf"] != "7777"{
-        t.Errorf("Expected cpf to be '7777'. Got '%v'", m["cpf"])
+    if m["cpf"] != "5555"{
+        t.Errorf("Expected cpf to be '5555'. Got '%v'", m["cpf"])
     }
 
     // O id é comparado com 1.0 porque o JSON unmarshaling converte números para
     // floats, quando usa o map[string]interface{}
-    if m["ID"] != 1.0 {
-        t.Errorf("Expected product ID to be '1'. Got '%v'", m["ID"])
+    if m["ID"] != 3.0 {
+        t.Errorf("Expected ID to be '3'. Got '%v'", m["ID"])
     }
+}
+
+//Testa rota GET /accounts, que retorna todas as contas cadastradas
+func TestGetAllAccounts(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/accounts", nil)
+	req.Header.Set("Content-Type", "application/json")
+
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+//Testa a rota /deposits que faz um depósito
+func TestNewDeposit(t *testing.T) {
+
+	//Simulamos a criação de um depósito
+	var jsonDep = []byte(`{ "ID" : "", "cpf" : "5555", "account_destination_id" : 3, "amount" : 10.0, "created_at" : ""}`)
+	req, _ := http.NewRequest("POST", "/deposits", bytes.NewBuffer(jsonDep))
+	req.Header.Set("Content-Type", "application/json")
+
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	var m string
+    json.Unmarshal(response.Body.Bytes(), &m)
+
+	fmt.Println(m)	
+}
+
+//Testa a rota de adquirir balance de uma conta
+func TestGetBalance(t *testing.T) {
+
+	//Chamamos a rota para o id 2
+	req, _ := http.NewRequest("GET", "/accounts/3/balance", nil)
+
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+    var m float64
+    json.Unmarshal(response.Body.Bytes(), &m)
+    //printa o Balance da conta com o ID passado na solicitação
+    fmt.Println("Balance da conta com ID 3 é:", m)
 }
