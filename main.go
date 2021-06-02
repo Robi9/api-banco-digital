96c35f07f1479f7030a7b2ed0d14408f3b06d54d
Roberta Moreira <30378788+Robi9@users.noreply.github.com>
2021-06-02 17:25:32 -0300

    Correção de string de conexão do banco.

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ var mongoOnce sync.Once
 
 //Dados de configuração do BD
 const (
-    CONNECTIONSTRING = "mongodb://localhost:27017" //localhost//mongodb
+    CONNECTIONSTRING = "mongodb://mongodb:27017" 
     DB               = "api-banco-digital"
     ACCOUNT          = "accounts"
     TRANSFER         = "transfers"
